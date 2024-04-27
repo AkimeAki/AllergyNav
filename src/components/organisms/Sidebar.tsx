8d0d9779abff27bd0026e9afb5c1fe9bf264374d
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-04-27 12:42:28 +0900

    スクロールの挙動修正

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -14,7 +14,7 @@ export default function (): JSX.Element {
 	const sidebarRef = useRef(null);
 	const pathname = usePathname();
 	const { userStatus, userId, userRole } = useGetUserData();
-	const { stopScroll, startScroll } = useScroll();
+	const { stopScroll, startScroll, isScroll } = useScroll();
 	const [isSizeSp, setIsSizeSp] = useState<boolean>(false);
 
 	const sidebarCloseAreaClick = (event: MouseEvent): void => {
@@ -45,7 +45,7 @@ export default function (): JSX.Element {
 	useEffect(() => {
 		if (isSidebarOpen && isSizeSp) {
 			stopScroll();
-		} else {
+		} else if (!isScroll) {
 			startScroll();
 		}
 	}, [isSizeSp, isSidebarOpen]);
