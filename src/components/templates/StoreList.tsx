4582c4078d37cda441c7ecfa5e82488a1d4b5f3d
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-04-27 12:53:39 +0900

    ロード処理修正

--- src/components/templates/StoreList.tsx
+++ src/components/templates/StoreList.tsx
@@ -178,7 +178,7 @@ export default function (): JSX.Element {
 							</div>
 						</div>
 					)}
-				{(getAllergensStatus === "loading" || getAllergensStatus === "yet") && <LoadingCircleCenter />}
+				{(getStoresStatus === "loading" || getStoresStatus === "yet") && <LoadingCircleCenter />}
 				<section
 					className={css`
 						display: grid;
