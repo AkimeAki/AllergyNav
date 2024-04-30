e0b935a3e4289770b689c85a474290269d6d570e
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-05-01 04:26:39 +0900

    gtag埋め込み

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,8 +42,26 @@ export default function ({ children }: Props): JSX.Element {
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6914867149724943"
 					crossOrigin="anonymous"
 				/>
+				{/* eslint-disable-next-line react/jsx-no-comment-textnodes, @next/next/next-script-for-ga */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+								new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+								j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+								'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+								})(window,document,'script','dataLayer','GTM-523PDB8H');`
+					}}
+				/>
 			</head>
 			<body>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-523PDB8H"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					/>
+				</noscript>
 				<KumaRegistry>{children}</KumaRegistry>
 			</body>
 		</html>
