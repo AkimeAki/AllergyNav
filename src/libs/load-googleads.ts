ac53cb4455ab001a029281a71d32f9c7075600a2
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-05-16 11:48:10 +0900

    test

--- src/libs/load-googleads.ts
+++ src/libs/load-googleads.ts
@@ -2,10 +2,5 @@ export const loadGoogleAds = (): void => {
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-expect-error
 	// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-	if (adsbygoogle && !adsbygoogle.loaded) {
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-expect-error
-		// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-		(adsbygoogle = window.adsbygoogle || []).push({});
-	}
+	(adsbygoogle = window.adsbygoogle || []).push({});
 };
