e94c5da093c8fc12fbe0104db0840ed237b52981
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-05-22 01:49:37 +0900

    テスト

--- src/libs/load-googleads.ts
+++ src/libs/load-googleads.ts
@@ -1,12 +1,18 @@
 export const loadGoogleAds = (): void => {
+	const root = document.querySelector("#root") as HTMLDivElement;
+	const adsNum = (root.dataset.adsNum ?? "") === "" ? 0 : Number(root.dataset.adsNum);
+
 	try {
 		setTimeout(() => {
 			if (process.env.NODE_ENV === "production") {
+				const currentAdsNum = (root.dataset.adsNum ?? "") === "" ? 0 : Number(root.dataset.adsNum);
+				console.log(adsNum, currentAdsNum);
+
 				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 				// @ts-expect-error
 				// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
 				(adsbygoogle = window.adsbygoogle || []).push({});
 			}
-		}, 500);
+		}, 1000);
 	} catch (e) {}
 };
