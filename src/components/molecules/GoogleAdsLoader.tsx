4b81f4edc8b187278dfb72c36539be96ac556d12
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-05-22 16:58:37 +0900

    広告処理テスト

--- src/components/molecules/GoogleAdsLoader.tsx
+++ src/components/molecules/GoogleAdsLoader.tsx
@@ -9,14 +9,12 @@ export default function (): JSX.Element {
 			mutations.forEach((mutation) => {
 				if (mutation.attributeName === "data-ads-num") {
 					try {
-						setTimeout(() => {
-							if (process.env.NODE_ENV === "production") {
-								// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-								// @ts-expect-error
-								// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-								(adsbygoogle = window.adsbygoogle || []).push({});
-							}
-						}, 500);
+						if (process.env.NODE_ENV === "production") {
+							// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+							// @ts-expect-error
+							// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+							(adsbygoogle = window.adsbygoogle || []).push({});
+						}
 					} catch (e) {}
 				}
 			});
