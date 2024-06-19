c579fad3c8f46ba01cb66447e826697279fabf4f
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-06-19 12:43:37 +0900

    googleマップの住所をエンコード

--- src/components/molecules/GoogleMap.tsx
+++ src/components/molecules/GoogleMap.tsx
@@ -8,7 +8,7 @@ export default function ({ search }: Props): JSX.Element {
 	return (
 		<>
 			<iframe
-				src={`https://maps.google.co.jp/maps?q=${search}&output=embed&t=m&z=17`}
+				src={`https://maps.google.co.jp/maps?q=${encodeURIComponent(search)}&output=embed&t=m&z=17`}
 				className={css`
 					position: relative;
 					z-index: -1;
