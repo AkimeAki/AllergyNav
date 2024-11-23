3874a8af089b71d43876cf8f9dd98cd1d7c6c9cf
Aki <62400162+AkimeAki@users.noreply.github.com>
2024-11-23 19:33:11 +0900

    スクロール位置修正処理の修正

--- src/components/molecules/ScrollTop.tsx
+++ src/components/molecules/ScrollTop.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 export default function (): JSX.Element {
 	const pathname = usePathname();
+	const searchParams = useSearchParams();
 
 	useEffect(() => {
 		const root = document.querySelector("#root") as HTMLDivElement;
 		root.scrollTo(0, 0);
-	}, [pathname]);
+	}, [pathname, searchParams]);
 
 	return <></>;
 }
