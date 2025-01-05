68d8deeac1e02b9595ad0d3da5fe37a7c47a0f79
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-01-05 16:05:59 +0900

    ScrollTopコンポーネントのuseEffect内での条件を修正し、Safariブラウザの判定を改善。useSearchParamsフックを追加。

--- src/components/organisms/ScrollTop.tsx
+++ src/components/organisms/ScrollTop.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function () {
 	const pathname = usePathname();
+	const params = useSearchParams();
 
 	useEffect(() => {
-		if (document.body.dataset.browser !== undefined && document.body.dataset.browser === "safari") {
-			window.scrollTo(0, 0);
+		window.scrollTo(0, 0);
 
-			setTimeout(() => {
-				window.scrollTo(0, 0);
-			}, 25);
-		}
-	}, [pathname]);
+		setTimeout(() => {
+			window.scrollTo(0, 0);
+		}, 25);
+	}, [pathname, params]);
 
 	return <></>;
 }
