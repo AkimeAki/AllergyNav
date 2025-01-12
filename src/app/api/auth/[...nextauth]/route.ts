21c61b261c59e884a7353aa6b339b684e7a91083
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-01-12 21:33:26 +0900

    NextAuthハンドラーを修正し、x-forwarded-hostヘッダーの設定を環境変数から取得するように変更

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,11 @@
 import { handler } from "@/libs/auth";
-export { handler as GET, handler as POST };
+import { NextRequest } from "next/server";
+
+const _handler = async (req: NextRequest, body: NextRequest["body"]) => {
+	req.headers.set("x-forwarded-host", process.env.NEXTAUTH_URL ?? req.headers.get("x-forwarded-host") ?? "");
+	const response = handler(req, body);
+	const res = await response;
+	return res;
+};
+
+export { _handler as GET, _handler as POST };
