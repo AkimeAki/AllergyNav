5ae8f322020e6b1529a18372abd388c1d25328ea
Aki <62400162+AkimeAki@users.noreply.github.com>
2025-01-12 21:51:48 +0900

    NextAuthハンドラーを簡素化し、x-forwarded-hostヘッダーの設定を削除

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,2 @@
 import { handler } from "@/libs/auth";
-import { NextRequest } from "next/server";
-
-const _handler = async (req: NextRequest, body: NextRequest["body"]) => {
-	const _req = req;
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-expect-error
-	_req.headers["x-forwarded-host"] = process.env.NEXTAUTH_URL || _req.headers["x-forwarded-host"];
-	const response = handler(_req, body);
-	const res = await response;
-	return res;
-};
-
-export { _handler as GET, _handler as POST };
+export { handler as GET, handler as POST };
